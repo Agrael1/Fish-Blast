e9437248b4a5215cb6d16e9e412230b03083cdf7
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-03-13 11:14:14 -0400

    made some crayon art for mouse cursor

--- autoload/debug_console/debug_console.gd
+++ autoload/debug_console/debug_console.gd
@@ -4,9 +4,7 @@ extends Node
 @onready var console: Control = $Console
 
 func _ready() -> void:
-	# HACK: the console has to do with mouse mode and it always instantiates at the
-	# beginning of the game, so the initial mouse state is set here. this is a global thing
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 	GsomConsole.toggled.connect(func(new_is_visible: bool) -> void:
 		console.visible = new_is_visible
@@ -14,11 +12,6 @@ func _ready() -> void:
 			TimeControl.pause_game()
 		else:
 			TimeControl.unpause_game()
-		# switch to captured mode if we close the terminal
-		if not new_is_visible:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		else:
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		)
 
 	GsomConsole.register_cvar("movespeed", 350.0, "Floating point scalar value for the maximum speed reachable with WASD")
